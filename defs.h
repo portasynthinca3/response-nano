a7d6fb966d427281c951f7cfcebcbd9a4f5d56a3
portasynthinca3 <portasynthinca3@gmail.com>
2020-02-03 19:57:48 +0300

    First codeful commit

---
+++ defs.h
@@ -0,0 +1,7 @@
+//Response Nano Firmware
+//By portasynthinca3 for Lithium Aerospace
+
+//Definitions
+
+#define SERIAL_BAUD_RATE                            115200
+#define SERIAL_GREETINGS                            ">Is anyone there?\n>Oh.\n>Hi!_\n\n---=== Response Nano firmware is running on this chip ===---"
---
+++ response-nano.ino
@@ -0,0 +1,16 @@
+//Response Nano Firmware
+//By portasynthinca3 for Lithium Aerospace
+
+//Main file
+
+//Include the libraries
+#include "defs.h"
+
+void setup(){
+    Serial.begin(SERIAL_BAUD_RATE);
+    Serial.println(SERIAL_GREETINGS);
+}
+
+void loop(){
+
+}
