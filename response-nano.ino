af67589dcdac1fce195e334d0b3860fc432e1604
portasynthinca3 <portasynthinca3@gmail.com>
2020-02-03 20:16:08 +0300

    Add license

--- response-nano.ino
+++ response-nano.ino
@@ -5,10 +5,16 @@
 
 //Include the libraries
 #include "defs.h"
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 
 void setup(){
+    //Initialize the serial port
     Serial.begin(SERIAL_BAUD_RATE);
+    //Print a greeting message
     Serial.println(SERIAL_GREETINGS);
+    //
 }
 
 void loop(){
