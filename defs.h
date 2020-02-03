3bae1715b6c68ee74da30013be3a91abbab6d245
portasynthinca3 <portasynthinca3@gmail.com>
2020-02-03 21:05:30 +0300

    LED status indicator

--- defs.h
+++ defs.h
@@ -1,7 +1,17 @@
 //Response Nano Firmware
 //By portasynthinca3 for Lithium Aerospace
 
-//Definitions
+//System-wide necessary definitions
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
 
+//Settings
 #define SERIAL_BAUD_RATE                            115200
 #define SERIAL_GREETINGS                            ">Is anyone there?\n>Oh.\n>Hi!_\n\n---=== Response Nano firmware is running on this chip ===---"
+
+//Pins
+#define PIN_LED                                     5
+
+//State definitions
+#define STATE_IDLE                                  0
--- response-nano.ino
+++ response-nano.ino
@@ -5,16 +5,36 @@
 
 //Include the libraries
 #include "defs.h"
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
 #include <Wire.h>
+
+uint32_t global_state;
+
+void led_pulse(int level, uint32_t time){
+    digitalWrite(PIN_LED, !level);
+    delay(time);
+}
+
+void led_blink_task(void* args){
+    pinMode(PIN_LED, OUTPUT);
+    while(1){
+        switch(global_state){
+            case STATE_IDLE:
+                led_pulse(HIGH, 100);
+                led_pulse(LOW, 1900);
+                break;
+        }
+    }
+}
 
 void setup(){
     //Initialize the serial port
     Serial.begin(SERIAL_BAUD_RATE);
     //Print a greeting message
     Serial.println(SERIAL_GREETINGS);
-    //
+    //Reset the state
+    global_state = STATE_IDLE;
+    //Create an LED blinking task
+    xTaskCreateUniversal(&led_blink_task, "LedBlinkTask", 1024, NULL, 7, NULL, 0);
 }
 
 void loop(){
