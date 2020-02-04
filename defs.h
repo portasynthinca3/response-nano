3a064fed3d12ec9205ed5ff9d663d8abcf79bb4f
portasynthinca3 <portasynthinca3@gmail.com>
2020-02-04 15:07:58 +0300

    Aborting and BMP180 readout

--- defs.h
+++ defs.h
@@ -8,10 +8,13 @@
 
 //Settings
 #define SERIAL_BAUD_RATE                            115200
-#define SERIAL_GREETINGS                            ">Is anyone there?\n>Oh.\n>Hi!_\n\n---=== Response Nano firmware is running on this chip ===---"
+#define SERIAL_GREETINGS                            F(">Is anyone there?\n>Oh.\n>Hi!_\n---=== Response Nano firmware is running ===---")
 
 //Pins
 #define PIN_LED                                     5
+#define PIN_I2C_SDA                                 21
+#define PIN_I2C_SCL                                 22
 
 //State definitions
 #define STATE_IDLE                                  0
+#define STATE_CRASH                                 1
--- response-nano.ino
+++ response-nano.ino
@@ -6,37 +6,81 @@
 //Include the libraries
 #include "defs.h"
 #include <Wire.h>
+#include <Adafruit_BMP085.h>
 
+//Hardware library objects
+Adafruit_BMP085 bmp;
+
+//Global computer state
 uint32_t global_state;
 
 void led_pulse(int level, uint32_t time){
+    //Set a level on the LED pin
+    //(it's active low, so invert)
     digitalWrite(PIN_LED, !level);
+    //Wait 'time' milliseconds
     delay(time);
 }
 
 void led_blink_task(void* args){
+    //Set the LED pin as output
     pinMode(PIN_LED, OUTPUT);
     while(1){
+        //Depending on the computer's state
+        //  blink a certain pattern
         switch(global_state){
             case STATE_IDLE:
-                led_pulse(HIGH, 100);
-                led_pulse(LOW, 1900);
+                led_pulse(HIGH, 50);
+                led_pulse(LOW, 1950);
+                break;
+            case STATE_CRASH:
+                for(int i = 0; i < 4; i++){
+                    led_pulse(HIGH, 250);
+                    led_pulse(LOW, 250);
+                }
+                delay(1000);
                 break;
         }
     }
 }
 
+void rn_abort(String message, String file, int line){
+    //Error out
+    //(worse case scenario, hopefully this will not happen during flight :<)
+    Serial.println(F("---=== RESPONSE NANO CRASH REPORT ===---"));
+
+    Serial.print(F("An unrecovable error has occured in the firmware at the following location:\n  in file "));
+    Serial.print(file);
+    Serial.print(F(" at line "));
+    Serial.println(line);
+
+    Serial.println(message);
+
+    //Set the "crash" state
+    global_state = STATE_CRASH;
+    //Hang
+    while(1);
+}
+
 void setup(){
     //Initialize the serial port
     Serial.begin(SERIAL_BAUD_RATE);
-    //Print a greeting message
     Serial.println(SERIAL_GREETINGS);
+
     //Reset the state
     global_state = STATE_IDLE;
     //Create an LED blinking task
-    xTaskCreateUniversal(&led_blink_task, "LedBlinkTask", 1024, NULL, 7, NULL, 0);
+    xTaskCreateUniversal(&led_blink_task, "LedBlinkTask", 512, NULL, 6, NULL, 0);
+
+    //Initialize the hardware
+    Wire.begin(PIN_I2C_SDA, PIN_I2C_SCL);
+    if(!bmp.begin())
+        rn_abort("Barometer initialization failed", __FILE__, __LINE__);
 }
 
 void loop(){
-
+    Serial.print("Reading alt... ");
+    float alt = bmp.readAltitude();
+    Serial.println(alt);
+    delay(500);
 }
